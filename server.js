3a4a36bad4efdc9475d18bcb19ea1515900dddaa
ekukwa <easmsm@gmail.com>
2022-08-09 11:27:21 -0400

    added heroku app and port

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.get('/api/animals', (req,res) => {
-//     res.json(animals);
-// });
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -52,6 +50,6 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
